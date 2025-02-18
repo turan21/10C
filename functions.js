cabbd43f320f802927f44268d7088839f9593c4d
Yxochistka <kutmanesen692@gmail.com>
2025-02-18 11:53:10 +0600

    Update and rename task.js to functions.js

---
+++ functions.js
@@ -0,0 +1,44 @@
+function greet(name) {
+    console.log(Hello, ${name});
+}
+
+function sum(a, b) {
+    return a + b;
+}
+
+function multiply(a, b, c) {
+    return a * b * c;
+}
+
+function isEven(num) {
+    return num % 2 === 0;
+}
+
+function doubleArray(arr) {
+    return arr.map(num => num * 2);
+}
+
+function factorial(n) {
+    if (n === 0 || n === 1) return 1;
+    return n * factorial(n - 1);
+}
+
+function getMax(a, b) {
+    return a > b ? a : b;
+}
+
+function reverseString(str) {
+    return str.split('').reverse().join('');
+}
+
+function isPalindrome(str) {
+    let reversed = str.split('').reverse().join('');
+    return str === reversed;
+}
+
+function filterOddNumbers(arr) {
+    return arr.filter(num => num % 2 !== 0);
+}
+
+
+
