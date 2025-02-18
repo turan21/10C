4ac064d4e952a06ccff565566ea3eaff0c3eb950
atake77 <atkknv15@gmail.com>
2025-02-18 11:29:49 +0600

    кеанпрол

---
+++ functions.js
@@ -0,0 +1,45 @@
+function greet(name) {
+  console.log(`Hello,  Atake`);
+}
+
+function sum(a, b) {
+  return a + b;
+}
+
+function multiply(a, b, c) {
+  return a * b * c;
+}
+
+function isEven(num) {
+  return num % 2 === 0;
+}
+
+function doubleArray(arr) {
+  return arr.map((num) => num * 2);
+}
+
+function factorial(n) {
+  if (n === 0 || n === 1) return 1;
+  let result = 1;
+  for (let i = 2; i <= n; i++) {
+    result *= i;
+  }
+  return result;
+}
+
+function getMax(a, b) {
+  return a > b ? a : b;
+}
+
+function reverseString(str) {
+  return str.split("").reverse().join("");
+}
+
+function isPalindrome(str) {
+  let reversed = reverseString(str);
+  return str === reversed;
+}
+
+function filterOddNumbers(arr) {
+  return arr.filter((num) => num % 2 !== 0);
+}
