f3e60f66437b13611bf7199f8ae8ede127edcc98
Yxochistka <kutmanesen692@gmail.com>
2025-02-18 11:47:01 +0600

    Create task.js

---
+++ task.js
@@ -0,0 +1,97 @@
+function greet(name) {
+    console.log(Hello, ${name});
+}
+
+function sum(a, b) {
+    return a + b;
+}
+
+function multiply(a, b, c) {
+    return a * b * c;
+}
+
+function isEven(num) {
+    return num % 2 === 0;
+}
+
+function doubleArray(arr) {
+    return arr.map(num => num * 2);
+}
+
+function factorial(n) {
+    if (n === 0 || n === 1) return 1;
+    return n * factorial(n - 1);
+}
+
+function getMax(a, b) {
+    return a > b ? a : b;
+}
+
+function reverseString(str) {
+    return str.split('').reverse().join('');
+}
+
+function isPalindrome(str) {
+    let reversed = str.split('').reverse().join('');
+    return str === reversed;
+}
+
+function filterOddNumbers(arr) {
+    return arr.filter(num => num % 2 !== 0);
+}
+
+
+let person = {
+    firstName: "Kutya",
+    lastName: "Essentuki",
+    age: 16
+};
+
+function updateAge(person, newAge) {
+    person.age = newAge;
+}
+
+let book = {
+    title: "Война и мир",
+    author: "Лев Николаевич Толстой",
+    yearPublished: 1949
+};
+
+function getBookInfo(book) {
+    console.log(The book ${book.title} was written by ${book.author} in ${book.yearPublished}.);
+}
+
+let car = {
+    brand: "Lambandjerry",
+    model: "kuraga",
+    year: 2010
+};
+
+function hasProperty(obj, prop) {
+    return obj.hasOwnProperty(prop);
+}
+
+let student = {
+    name: "Azim",
+    subjects: ["Math", "Russian"],
+    grades: {
+        Math: "A",
+        Russian: "B"
+    }
+};
+
+function addSubject(student, newSubject) {
+    if (!student.subjects.includes(newSubject)) {
+        student.subjects.push(newSubject);
+    }
+}
+
+let city = {
+    name: "Moscow",
+    country: "Russia",
+    population: 2148000
+};
+
+function describeCity(city) {
+    console.log(The city ${city.name} is located in ${city.country} and has a population of ${city.population}.);
+}
